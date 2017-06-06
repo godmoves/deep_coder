64eca33f226419857e4952e78583ab0f498f2152
godmoves <514158072@qq.com>
2017-06-06 21:03:25 +0800

    update code

--- c_code/train.py
+++ c_code/train.py
@@ -8,7 +8,7 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 import time
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from model import Model
 from logger import Logger
 
