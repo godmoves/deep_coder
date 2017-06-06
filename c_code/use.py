50c604e656268a6fdb012e8e618edae2b4b4833a
pwjtc <514158072@qq.com>
2017-06-06 20:51:10 +0800

    upgrade

--- c_code/use.py
+++ c_code/use.py
@@ -9,6 +9,9 @@ Options:
   -h --help                 Show this help.
   <ckpt>                    Path to the ckpt file to use
 """
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
 import tensorflow as tf
 import numpy as np
